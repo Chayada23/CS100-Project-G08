2ef250892ef5d1bcd8dbe0125585898287d92a8d
Nammon <thawanhathai.thi@dome.tu.ac.th>
2023-11-28 18:15:49 +0700

    css + alert js ,new header

--- script.js
+++ script.js
@@ -17,6 +17,20 @@ function submitForm(event) {
   // Attach a submit event listener to the form
   document.getElementById("myForm").addEventListener("submit", submitForm);
   
-  function showAlert(name) {
+  /*function showAlert(name) {
     alert('คุณคลิกที่ ' + name + ' แล้ว');
+  }*/
+
+  function showNuchAlert() {
+    alert('talk to Nuch ♡︎ : ไม่จำเป็นต้องแข่งขัน เพราะเรามันไม่ใช่ไก่ ');
   }
+
+  function showNammonAlert() {
+    alert('talk to Nammon ♡︎ : ในวันที่เราล้ม เข่าเราจะถลอก');
+  }
+
+  function showNamfonAlert() {
+    alert('♡talk to Namfon ♡︎ : Look just good things and grow hapily every day');
+  }
+
+
